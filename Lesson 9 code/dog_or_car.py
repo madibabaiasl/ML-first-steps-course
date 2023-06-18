1075b7918f3682bfab340d7f36fa6e4ef77feeab
Madi Babaiasl <118206851+madibabaiasl@users.noreply.github.com>
2023-06-17 21:00:06 -0500

    Create dog_or_car.py

---
+++ Lesson 9 code/dog_or_car.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 03:23:24 2022
+
+@author: mecharithm
+"""
+# Import the necessary libraries
+import os
+import numpy as np
+from PIL import Image
+from numpy import asarray, empty, append, array
+
+def dataset_collect():
+    # Initialize training and testing datasets
+    train_X = empty((0, 60, 60, 3), int)
+    train_y = array([])
+    test_X = empty((0, 60, 60, 3), int)
+    test_y = array([])
+    
+    # get the path/directory for training dataset & load it
+    folder_dir = os.getcwd() + '/train'
+    for images in os.listdir(folder_dir):
+        # check if the image ends with jpg
+        if (images.endswith(".jpg")):
+            img = Image.open(os.getcwd() + '/train/' + images) #--> Open image
+            img = img.resize((60, 60)) #--> Resize image to a reasonable size
+            if (asarray(img).shape == (60, 60, 3)):
+                train_X = append(train_X, [asarray(img)], axis = 0) #--> Convert to numpy array
+                if (images.startswith("n0")):
+                    train_y = append(train_y, 1)
+                else:
+                    train_y = append(train_y, 0)
+                
+    # get the path/directory for testing dataset & load it
+    folder_dir = os.getcwd() + '/test'
+    for images in os.listdir(folder_dir):
+        # check if the image ends with jpg
+        if (images.endswith(".jpg")):
+            img = Image.open(os.getcwd() + '/test/' + images) #--> Open image
+            img = img.resize((60, 60)) #--> Resize image to a reasonable size
+            if (asarray(img).shape == (60, 60, 3)):
+                test_X = append(test_X, [asarray(img)], axis = 0) #--> Convert to numpy array
+                if (images.startswith("n0")):
+                    test_y = append(test_y, 1)
+                else:
+                    test_y = append(test_y, 0)
+                
+    # Reshape labels
+    #train_y = train_y.reshape(train_y.shape[0], 1).T
+    #test_y = test_y.reshape(test_y.shape[0], 1).T
+                
+    return train_X, np.int64(train_y), test_X, np.int64(test_y)
